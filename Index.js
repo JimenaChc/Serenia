6bc75fda39d0de15c0bcae013dc14badb79bdc3e
JimenaChc <jimenachaconcalderon@gmail.com>
2025-12-06 16:24:41 -0600

    Update Index.js

--- Index.js
+++ Index.js
@@ -13,7 +13,7 @@ import RutaCambio from "./Rutas/RutaCambio.js"
 dotenv.config();
 const app = express();
 app.use(cors());
-app.use(express.static("Public")); 
+app.use(express.static("public")); 
 
 app.get("/", (req, res) => {
   res.sendFile("Login.html", { root: "public" });
