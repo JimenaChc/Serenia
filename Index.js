508f6787f475b125eaf7012a7ce53643e26ea6bd
JimenaChc <jimenachaconcalderon@gmail.com>
2025-12-06 16:21:24 -0600

    Update Index.js

--- Index.js
+++ Index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.static("Public")); 
 
 app.get("/", (req, res) => {
-  res.sendFile("Login.html", { root: "Public" });
+  res.sendFile("Login.html", { root: "public" });
 });
 
 app.use(express.json({ limit: "50mb" }));
