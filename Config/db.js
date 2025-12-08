5b0057a584d65380258879d9ed4dbe756c2dd3d8
JimenaChc <jimenachaconcalderon@gmail.com>
2025-12-08 11:19:09 -0600

    Update db.js

--- Config/db.js
+++ Config/db.js
@@ -20,12 +20,4 @@ const conexion = {
   }
 };
 
-conexion.connect((err) => {
-  if (err) {
-    console.error("Error de conexión a la base de datos ", err);
-  } else {
-    console.log("Conectado a la base de datos");
-  }
-});
-
 export default conexion;
