b6caa62fdc8d4923e396e34a6b3dbd9e8b2a6875
JimenaChc <jimenachaconcalderon@gmail.com>
2025-12-10 12:28:32 -0600

    Update db.js

--- Config/db.js
+++ Config/db.js
@@ -5,19 +5,21 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : undefined,
   ssl: { rejectUnauthorized: false },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  allowPublicKeyRetrieval: true,
+
 });
 
-const conexion = {
-  query: async (sql, params) => {
+const conexion = 
+  query: async (sql, params = []) => {
     const [rows] = await pool.query(sql, params);
     return rows;
-  }
+  },
+
+  pool,
 };
 
 export default conexion;
